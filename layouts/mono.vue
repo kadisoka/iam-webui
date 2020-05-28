2d7a46924786fa7e25857a20c767286ae93aed4f
Fahrezal Effendi <exavolt@gmail.com>
2020-05-28 11:02:59 +0000

    Init

---
+++ layouts/mono.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="layout-mono has-background-light">
+    <div class="content">
+      <div>
+        <nuxt />
+        <div>Home</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.layout-mono > div.content {
+  text-align: center;
+  display: flex;
+  min-height: 100vh;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ pages/signin/identifier.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-title">TODO: LOGO</p>
+    </div>
+    <div class="card-content">
+      <h2>
+        Sign in
+      </h2>
+      <div>
+        <b-field label="Email or phone">
+          <b-input value="user@example.com"></b-input>
+        </b-field>
+        <div class="buttons is-right">
+          <button class="button is-primary">Next</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'mono'
+}
+</script>
+
+<style>
+.card {
+  min-width: 360px;
+}
+</style>
