b1cd914fc9cc2de971fc85775d95881a858855c7
Fahrezal Effendi <exavolt@gmail.com>
2020-05-28 23:32:38 +0700

    Update the main navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,30 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
+    <b-navbar
+      class=" header has-shadow is-primary"
       role="navigation"
       aria-label="main navigation"
     >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
+      <template slot="brand">
+        <b-navbar-item href="/">
           <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <b-navbar-item
-            v-if="$store.state.iam.accessToken"
-            to="/signout"
-            tag="router-link"
-            >Sign out</b-navbar-item
-          >
-          <b-navbar-item v-else to="/signin" tag="router-link"
-            >Sign in</b-navbar-item
-          >
-        </div>
-      </div>
-    </nav>
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-navbar-item to="/accounts/settings" tag="router-link"
+          >Account</b-navbar-item
+        >
+        <b-navbar-item
+          v-if="$store.state.iam.accessToken"
+          to="/signout"
+          tag="router-link"
+          >Sign out</b-navbar-item
+        >
+        <b-navbar-item v-else to="/signin" tag="router-link"
+          >Sign in</b-navbar-item
+        >
+      </template>
+    </b-navbar>
 
     <section class="main-content">
       <nuxt />
